975bf9365c91ddcdc391e372cd3eb0a317653d05
zxycw <yigeyige20@gmail.com>
2026-02-13 18:52:43 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 
-  import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
