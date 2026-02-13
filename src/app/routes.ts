e6c2b43a6e07cb0bc84c79b6d3ad59a11c6b1ec9
zxycw <yigeyige20@gmail.com>
2026-02-13 18:15:00 +0800

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,16 +6,16 @@ import Analysis from "@/app/pages/Analysis";
 import Guide from "@/app/pages/Guide";
 import About from "@/app/pages/About";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: "education", Component: Education },
-      { path: "analysis", Component: Analysis },
-      { path: "guide", Component: Guide },
-      { path: "about", Component: About },
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Root,
+      children: [
+        { index: true, Component: Home },
+        { path: "education", Component: Education },
+        { path: "analysis", Component: Analysis },
+        { path: "guide", Component: Guide },
+        { path: "about", Component: About },
       ],
     },
   ],
